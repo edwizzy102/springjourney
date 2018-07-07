54b721a6870508fe3f7bfb747d5c86913a97d0aa
edwizzy102 <eddieekpo1992@Live.com>
2018-07-06 23:35:28 -0700

    Section 1

--- src/main/java/com/destinyhospicecare/spring5webapp/modal/Author.java
+++ src/main/java/com/destinyhospicecare/spring5webapp/modal/Author.java
@@ -2,6 +2,7 @@ package com.destinyhospicecare.spring5webapp.modal;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -63,5 +64,19 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
     }
 }
